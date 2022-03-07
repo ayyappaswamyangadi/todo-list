1901592cd30bb6535828de5776a77c7bf0a35de4
Ayyappa Swamy Angadi <ayyappaswamy50@gmail.com>
2022-03-07 22:46:48 +0530

    created todo react app

---
+++ todo-list/src/todo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function todo() {
+  return (
+    <div>todo</div>
+  )
+}
+
+import React from 'react'
+
+export const todo = () => {
+  return (
+    <div>todo</div>
+  )
+}
+
+import React, { Component } from 'react'
+
+export default class todo extends Component {
+  render() {
+    return (
+      <div>todo</div>
+    )
+  }
+}
